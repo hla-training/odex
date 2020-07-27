6e90ac7c8bd6fc4aacc0e998b6c203ce63df9fb8
MOHD RAZIF AZMAL BIN. MOHD OT <razifazmal@mampunet.btm>
2020-07-27 11:35:37 +0800

    razif edit 2

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -76,6 +76,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<br>
 	<p>HAI OD!!!!!..</p>
 	<p>HAI NASH!!!!!</p>
+
+	<p>HAI MEOWWW!!!!!</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
