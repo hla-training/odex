a9fe07c35afd47b37a5126df4e5ee095d4407433
Zaimar Binti Umar <zaimar@mampunet.btm>
2020-07-28 15:23:54 +0800

    xai

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -80,7 +80,7 @@ public function ckandataset()
 	CURLOPT_CUSTOMREQUEST => "POST",
 	CURLOPT_POSTFIELDS => $data,
 	CURLOPT_HTTPHEADER => array(
-	"Authorization: 6a7b52e5-28a5-42d9-a977-9a2e3bc26d22"
+	"Authorization: a09024a7-865c-4468-b3cf-bb7fa0889507"
 	),
 	));
 	$response = curl_exec($curl);
