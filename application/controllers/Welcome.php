57114fd01505ba2451d163a3b8c8a30d43ef0dc1
Nor Nasharyati Binti Nasir <nasharyati@mampunet.btm>
2020-07-28 15:03:50 +0800

    update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -27,4 +27,107 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('nash');
 	}
+
+	public function getdataapi()
+	{
+	$curl = curl_init();
+	$url = 'https://api.bnm.gov.my/public/base-rate/BKKBMYKL';
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => $url,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => array(
+	"Accept: application/vnd.BNM.API.v1+json",
+	"Access-Control-Allow-Origin: *"
+	),
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$results = json_decode($response, true);
+	if(strpos($url,'bnm') !== false){
+	$resultso = $results["data"];
+	} else {
+	$resultso = $results["result"]["records"];
+	}
+	if (array_key_exists(0,$resultso)){
+	$fp = fopen(getcwd().'/data/sample.csv', 'w');
+	fputcsv($fp,array_keys($resultso[0]));
+	foreach ($resultso as $result) {
+	if (fputcsv($fp, $result) === false) {
+	return false;
+	}
+	}
+	fclose($fp);
+	} else {
+	return false;
+	}
+	}
+
+	public function ckandataset()
+	{
+		$curl = curl_init();
+		$data = json_encode(array(
+		'name' => 'nama dataset',
+		'notes' => 'dataset',
+		'owner_org'=> 'organisasi',
+		));
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://demo.ckan.org/api/3/action/package_create",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "POST",
+		CURLOPT_POSTFIELDS => $data,
+		CURLOPT_HTTPHEADER => array(
+		"Authorization: 6a7b52e5-28a5-42d9-a977-9a2e3bc26d22"
+		),
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+		echo $response;
+		}	
+
+		public function ckanresource()
+		{
+		$curl = curl_init();
+		$data = json_encode(array(
+		'url' => 'http://103.253.145.248/zaimar/odex/data/sample.csv',
+		'name' => 'test name',
+		'description'=> 'descriptions',
+		'format'=>'csv',
+		'package_id' => 'package name'
+		));
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://demo.ckan.org/api/3/action/resource_create",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "POST",
+		CURLOPT_POSTFIELDS => $data,
+		CURLOPT_HTTPHEADER => array(
+		"Authorization: 6a7b52e5-28a5-42d9-a977-9a2e3bc26d22"
+		),
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+		echo $response;
+		}
+
+		public function datacleaning()
+		{
+		exec('python3 datacleaning.py');
+		}
+
+
 }
