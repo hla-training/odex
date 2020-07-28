db8df7f096a977f9edc8392f521241441b2a52a7
MOHD RAZIF AZMAL BIN. MOHD OT <razifazmal@mampunet.btm>
2020-07-28 17:10:45 +0800

    razif edit api

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -101,7 +101,7 @@ public function ckanresource2()
 $curl = curl_init();
 $data = json_encode(array(
 'url' => 'http://103.253.145.248/razif/odex/data/sample.csv',
-'name' => 'test name',
+'name' => 'testname',
 'description'=> 'descriptions',
 'format'=>'csv',
 'package_id' => 'package name'
